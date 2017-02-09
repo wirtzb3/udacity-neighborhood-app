6163e576e6ef396155ab47db066a7fd45085a7ea
Brittany Wirtz <brittanyjhanson@gmail.com>
2017-02-09 13:30:17 -0800

    Add index2.html and a few tweaks

---
+++ newapp.js
@@ -0,0 +1,31 @@
+var Model = function() {
+  var allLocations = [{
+      title: 'CenturyLink Field',
+      location: {lat: 47.5952, lng: -122.3316}
+    },{
+      title: 'Starfire Sports',
+      location: {lat:	47.470159, lng: -122.249129}
+    },{
+      title: 'Fuel Sports Bar',
+      location: {lat:	47.601236, lng: 	-122.331833}
+    },{
+      title: 'Occidental Square',
+      location: {lat: 47.600318, lng: -122.333388}
+    },{
+      title: 'Fado Irish Pub',
+      location: {lat: 47.604184, lng: -122.335175}
+    },{
+      title: 'Memorial Stadium',
+      location: {lat: 47.622815, lng:	-122.349530}
+    };
+  ];
+
+};
+
+var ViewModel = function() {
+  this
+};
+
+var View = function() {
+
+};
